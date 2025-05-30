0db6bab9e423b6806b7fa7bb8ea55818e1422425
Yassine Douhabi <douhabiyassine@gmail.com>
2025-05-30 14:14:54 +0100

    from windows

--- app/api/restaurants/route.js
+++ app/api/restaurants/route.js
@@ -1,6 +1,11 @@
-import { NextResponse } from "next/server";
-import { restaurants } from "../data/restaurants";
+import clientPromise from '@/lib/mongodb';
 
 export async function GET() {
-  return NextResponse.json(restaurants);
+  const client = await clientPromise;
+  const db = client.db('restaurants');
+  const posts = await db.collection('posts').find({}).toArray();
+
+  return new Response(JSON.stringify(posts), {
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
---
+++ app/lib/mongodb.js
@@ -0,0 +1,30 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  throw new Error('Please add your Mongo URI to .env.local');
+}
+
+const options = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+};
+
+let client;
+let clientPromise;
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
